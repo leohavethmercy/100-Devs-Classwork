aa5e9176b4a2198048863eae36af78f238b52a83
leohavethmercy <billiejefferys@gmail.com>
2022-03-22 21:37:27 -0400

    completed objects exercises

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,44 @@
 //Arrays
 
-//Create and array of tv shows. Loop through and print each show to the console
+//Create and array of tv shows. 
+//Loop through and print each show to the console
+
+let tvShows = ['Chappelle','MTV','VH1']
+
+tvShows.forEach((x) => console.log(x))
 
 //Create and array of numbers
-//Return a new array of numbers that includes every even number from the previous Arrays
+//Return a new array of numbers that includes every even
+// number from the previous Arrays
+
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// numbers.forEach(x =>)
+
+// let newNumbers = []
+
+// numbers.map(x => {
+//     if (x % 2 == 0){
+//         newNumbers.push(x)
+//     }
+// })
+
+// console.log(newNumbers)
+
+// .filter checks if true -- if true it includes in new array
+// implicit return - returned to where its called
+let newNumbers = arr => arr.filter(x => x%2 == 0)
+console.log(newNumbers(numbers))
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+
+function someNums(arr) {
+    //sorting numbers in an array -> small to high
+   let sorted = arr.sort((a,b) => a-b)
+   let min = sorted[1]
+   let max = sorted[sorted.length-2]
+   return min + max
+}
+
+console.log(someNums([1,2,3,4,5,6,7]))
--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,23 @@
-//Create a stopwatch object that has four properties and three methods
+//Create a stopwatch object that 
+//has four properties and three methods
+
+
+let stopwatch = {
+    color: 'blue',
+    weight: 1.5,
+    material: 'Gold',
+    currentTime: 4,
+    watchColor: function () {
+        console.log( `The watch color is ${this.color}`)
+    },
+    watchWeight: function () {
+        console.log(`The watch weight is ${this.weight}`)
+    },
+    watchMaterial: function () {
+        return `The watch material is ${this.material}`
+    },
+}
+
+stopwatch.watchWeight()
+stopwatch.watchColor()
+console.log(stopwatch.watchMaterial())

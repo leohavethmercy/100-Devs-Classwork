6211c8a67de970c8b966da0fe13c306c4fb1499a
leohavethmercy <billiejefferys@gmail.com>
2022-04-02 00:03:23 -0400

    Class 24 materials

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,25 @@
-//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a Tony Hawk Pro Skater constructor that
+// makes fighting game characters with 
+//4 properties and 3 methods
+
+function Skater(push,move,chStance,name) {
+    this.skate = push,
+    this.specialMove = move,
+    this.stance = chStance,
+    this.name = name,
+    this.ollie = function() {
+        return `${this.name} just Ollied!`
+    },
+    this.kickflip = function() {
+        return `${this.name} just Kickflipped!`
+    },
+    this.nollie = function() {
+        return `${this.name} just Nollied`
+    }
+
+
+}
+
+let Beasely = new Skater('Hard','Tre flip','lean','Beasely')
+
+console.log(Beasely.kickflip())

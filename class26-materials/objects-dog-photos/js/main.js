79ab3e72595b531bd2798c5878858d8ca7fe534f
leohavethmercy <billiejefferys@gmail.com>
2022-04-12 19:10:05 -0400

    Class 27 files

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,13 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+url = "https://dog.ceo/api/breeds/image/random"
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.message)
+        document.querySelector('img').src = data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
--- class26-materials/turbo-review/js/main.js
+++ class26-materials/turbo-review/js/main.js
@@ -62,3 +62,4 @@ printer(3)
 //t a bot using the above function
 
 
+

c1e55729ef20a4f05f6f7a23b4b8ad5f3a587364
leohavethmercy <billiejefferys@gmail.com>
2022-04-12 19:22:44 -0400

    Object constructor

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,30 @@
-//Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+//Create a Netflix TV Show class with a constructor 
+//that makes Netflix TV Shows with 4 properties and
+// 3 methods
 
 
+class MakeNetflixTvShows {
+    constructor(title, genre, rating, numOfEp) {
+        this.title = title
+        this.genre = genre
+        this.rating = rating
+        this.numOfEp = numOfEp
+    }
+
+    play() {
+        console.log('Playing..')
+    }
+    stop() {
+        console.log('Stopping..')
+    }
+    saveToList() {
+        console.log('Saved to list..')
+    }
+
+}
+
+const Naruto = new MakeNetflixTvShows('Naruto','Anime','R',1000);
+
+console.log(Naruto.title)
+console.log(Naruto.genre)
+Naruto.saveToList()

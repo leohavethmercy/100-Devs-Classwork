353ec2783ac6a7126afc4c5b87fb5b741befd25c
leohavethmercy <billiejefferys@gmail.com>
2022-04-12 20:52:36 -0400

    Localstorage functions

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -3,12 +3,24 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  console.log(choice)
+
+
+  const url =  `https://api.nasa.gov/planetary/apod?
+  api_key=XPoZ2SEQYeswVokUaUvgbtS07rZjspxfGAaeXTPg&date=${choice}`
+
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if (data.media_type === 'image') {
+          document.querySelector('img').src = data.hdurl
+
+        } else if (data.media_type === 'video') {
+          document.querySelector('iframe').src = data.hdurl
+        }
+        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
